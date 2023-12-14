94ca276053d2dbfa118b1295b5749cdd2dd6c4b7
Brendan McGaw <Brendanmcgaw@gmail.com>
2023-12-14 21:21:15 +1100

    Added some styling changes to the top-menu

--- src/Components/TopMenu.js
+++ src/Components/TopMenu.js
@@ -5,8 +5,14 @@ import Menu from '@mui/material/Menu'
 import MenuItem from '@mui/material/MenuItem'
 import styled from '@emotion/styled'
 
+// TODO: Should my styled components be in their own component?
+// installed vscode styled components extension for css syntax and suggestions.
 const StyledMenuButton = styled(MenuButton)`
     border-radius: 0;
+    color: white;
+    :hover {
+        background-color: orange;
+    }
 `
 
 export const TopMenu = () => {
@@ -14,62 +20,76 @@ export const TopMenu = () => {
         <Grid
             MenuItem
             className="headerMenu"
-            xl={12}
+            md={12}
             textAlign={'center'}
             backgroundColor={'#131417'}
-            direction={'row'}
+            display={'flex'}
+            justifyContent={'space-around'}
+            gap={2}
+            // alignItems={'flex-end'}
+            // alignSelf={'flex-end'}
+            // justifySelf={'flex-end'}
+            // Is the reason none of these work because they're not "Grid Items"?
         >
-            <Dropdown>
-                <StyledMenuButton variant="outlined" size="lg" color="primary">
-                    Keys
-                </StyledMenuButton>
-                <Menu>
-                    <MenuItem>Programmable</MenuItem>
-                    <MenuItem>Shaped</MenuItem>
-                </Menu>
-            </Dropdown>
-            <Dropdown>
-                <StyledMenuButton
-                    variant="outlined"
-                    size="lg"
-                    color="primary"
-                    Format
-                >
-                    Hardware
-                </StyledMenuButton>
-                <Menu>
-                    <MenuItem>Keys</MenuItem>
-                    <MenuItem>Other Things</MenuItem>
-                </Menu>
-            </Dropdown>
-            <Dropdown>
-                <StyledMenuButton
-                    variant="outlined"
-                    size="lg"
-                    color="primary"
-                    Format
-                >
-                    Devices
-                </StyledMenuButton>
-                <Menu>
-                    <MenuItem>Cars</MenuItem>
-                    <MenuItem>Machinary</MenuItem>
-                </Menu>
-            </Dropdown>
-            <Dropdown>
-                <StyledMenuButton
-                    variant="outlined"
-                    size="lg"
-                    color="primary"
-                    Format
-                >
-                    My Account
-                </StyledMenuButton>
-                <Menu>
-                    <MenuItem>Profile</MenuItem>
-                    <MenuItem>Logout</MenuItem>
-                </Menu>
-            </Dropdown>
+            <Grid item>
+                <Dropdown>
+                    <StyledMenuButton variant="plain" size="lg" color="primary">
+                        Keys
+                    </StyledMenuButton>
+                    <Menu>
+                        <MenuItem>Programmable</MenuItem>
+                        <MenuItem>Shaped</MenuItem>
+                    </Menu>
+                </Dropdown>
+            </Grid>
+            <Grid item>
+                <Dropdown>
+                    <StyledMenuButton
+                        variant="plain"
+                        size="lg"
+                        color="primary"
+                        Format
+                    >
+                        Hardware
+                    </StyledMenuButton>
+                    <Menu>
+                        <MenuItem>Keys</MenuItem>
+                        <MenuItem>Other Things</MenuItem>
+                    </Menu>
+                </Dropdown>
+            </Grid>
+            <Grid item>
+                <Dropdown>
+                    <StyledMenuButton
+                        variant="plain"
+                        size="lg"
+                        color="primary"
+                        Format
+                    >
+                        Devices
+                    </StyledMenuButton>
+                    <Menu>
+                        <MenuItem>Cars</MenuItem>
+                        <MenuItem>Machinary</MenuItem>
+                    </Menu>
+                </Dropdown>
+            </Grid>
+            <Grid item>
+                <Dropdown>
+                    <StyledMenuButton
+                        variant="plain"
+                        size="lg"
+                        color="primary"
+                        Format
+                    >
+                        My Account
+                    </StyledMenuButton>
+                    <Menu>
+                        <MenuItem>Profile</MenuItem>
+                        <MenuItem>Logout</MenuItem>
+                    </Menu>
+                </Dropdown>
+            </Grid>
         </Grid>
     )
 }
